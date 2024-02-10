737d89038dd097c4a22fd658c21d4e7e1ca08a0e
Yurii Havrylko <yurii.havrylko@gmail.com>
2024-02-10 22:42:07 +0100

    removed old handwriten code

--- revenuecat_python/__init__.py
+++
@@ -1,3 +0,0 @@
-from .client import RevenueCatClient  # noqa
-from .enums import AttributionNetworkCode, SubscriptionPlatform  # noqa
-from .responses import RevenueCatError, RevenueCatResponse  # noqa
